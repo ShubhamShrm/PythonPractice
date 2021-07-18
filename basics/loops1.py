e1f3767c2e01f53c94e63da3dca44bbb6c47e686
Shubham Sharma1 <shubham.sharma1@Brightlifecare.local>
2021-07-18 17:51:53 +0530

    ROTN

--- basics/loops1.py
+++ basics/loops1.py
@@ -5,3 +5,29 @@ Created on Sun Jul 18 15:15:17 2021
 @author: shubham.sharma1
 """
 
+
+def ROTnEnc(n, s):
+    l = list(s)
+    for i in range(n):
+        a = l.pop(-1)
+        l.insert(0,a)
+    return ''.join(l)
+
+
+
+def ROTnDec(n, s):
+    l = list(s)
+    for i in range(n):
+        a = l.pop(0)
+        l.append(a)
+    return ''.join(l)
+
+
+if __name__ == '__main__':
+    secretmsg = 'I am batman!'
+    n = 13
+    encmsg = ROTnEnc(n, secretmsg)
+    print(encmsg)
+    
+    decmsg = ROTnDec(n, encmsg)
+    print(decmsg)
---
+++ basics/rotn.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 18 16:49:41 2021
+
+@author: shubham.sharma1
+"""
+
+
+
+
+def Rotn(n,s):
+    a = ''
+    for i in s:
+        x = chr(ord(i) + n)
+        a = a + x
+    return(a)    
+     
+
+
+def Rotdec(n, a):
+    b= ''
+    for i in a:
+        y= chr(ord(i) - n)
+        # print(i, y)
+        b= b + y
+    return(b)    
+      
+        
+    
+    
+if __name__ == '__main__': 
+    import sys
+    try:
+        s = sys.argv[1]
+    except Exception:
+        s = input("Enter a message: ")
+        
+    n = 2
+    c = Rotn(n,s)
+    print(c)
+    d = Rotdec(n, c) 
+    print(d)
+        
+
+         
+
+    
+
+
+            
+        
